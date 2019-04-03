a5b573c7513b967b4e156d6a3c686ea6cdb6bd71
Agata Stawarz <agata.stawarz94@gmail.com>
2019-04-03 15:44:37 +0200

    Add Login feature

--- scripts/auth.js
+++ scripts/auth.js
@@ -14,10 +14,33 @@ signupForm.addEventListener('submit', (e) => {
         // Handle Errors here
         var errorMessage = error.message;
         alert(errorMessage)
-      });
+    });
     
     // let the modal close and reset the form
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
     signupForm.reset();
+}); 
+
+// login 
+const loginForm = document.querySelector('#login-form');
+signupForm.addEventListener('submit', (e) => {
+    // to prevent from page refresh
+    e.preventDefault(); 
+
+    // get info about the user (from the signup form)
+    const email = signupForm['login-email'].value
+    const password = signupForm['login-password'].value
+
+    // sign up the user
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here
+        var errorMessage = error.message;
+        alert(errorMessage)
+    });
+    
+    // let the modal close and reset the form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
 }); 
