fcf087a596d9ce496be1725b346610ab728e9460
Agata Stawarz <agata.stawarz94@gmail.com>
2019-04-03 15:29:16 +0200

    Add Sign up feature

---
+++ scripts/auth.js
@@ -0,0 +1,23 @@
+
+// sign up
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    // to prevent from page refresh
+    e.preventDefault(); 
+
+    // get info about the user (from the signup form)
+    const email = signupForm['signup-email'].value
+    const password = signupForm['signup-password'].value
+
+    // sign up the user
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here
+        var errorMessage = error.message;
+        alert(errorMessage)
+      });
+    
+    // let the modal close and reset the form
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+}); 
